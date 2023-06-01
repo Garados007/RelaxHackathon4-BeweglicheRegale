43e83a88c4b0678c402e9903382a9ea59f118502
Max Brauer <ma.brauer@live.de>
2023-06-02 00:32:38 +0200

    store move map

---
+++ src/Regale.Lib/MoveMap.cs
@@ -0,0 +1,56 @@
+using System.Text;
+using System.Text.Json;
+
+namespace Regale;
+
+public class MoveMap : Map<Direction>
+{
+    public MoveMap(int width, int height) : base(width, height)
+    {
+    }
+
+    public void Save(Utf8JsonWriter writer)
+    {
+        var sb = new StringBuilder(Width * 4 + 2);
+        writer.WriteStartArray();
+        var indent = Span<byte>.Empty;
+        var indentOffset = 0;
+        if (writer.Options.Indented)
+        {
+            indentOffset = writer.CurrentDepth * 4 + 2;
+            indent = new byte[indentOffset + sb.Capacity];
+            indent.Fill((byte)' ');
+            indent[0] = (byte)'\r';
+            indent[1] = (byte)'\n';
+        }
+        for (uint y = 0; y < Height; ++y)
+        {
+            var row = GetRow(y);
+            sb.Clear();
+            sb.Append('"');
+            for (int x = 0; x < row.Length; ++x)
+            {
+                sb.Append(row[x] switch
+                {
+                    Direction.None => "🟨",
+                    Direction.Up => "⏫",
+                    Direction.Right => "⏩",
+                    Direction.Down => "⏬",
+                    Direction.Left => "⏪",
+                    _ => throw new NotSupportedException($"{row[x]} is not supported"),
+                });
+            }
+            sb.Append('"');
+            var text = sb.ToString();
+            Span<byte> bytes = System.Text.Encoding.UTF8.GetBytes(text);
+            if (writer.Options.Indented)
+            {
+                bytes.CopyTo(indent[indentOffset..]);
+                writer.WriteRawValue(indent[..(indentOffset + bytes.Length)], false);
+            }
+            else writer.WriteRawValue(bytes, false);
+
+        }
+        writer.WriteEndArray();
+    }
+}
---
+++ src/Regale.Test/TestMoveMapSaving.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+
+namespace Regale.Test;
+
+#pragma warning disable NUnit2007, NUnit2005, NUnit2003
+
+public class TestMoveMapSaving
+{
+    private static string Store(Direction[][] dirs)
+    {
+        var map = new MoveMap(dirs[0].Length, dirs.Length);
+        for (int y = 0; y < dirs.Length; ++y)
+            for (int x = 0; x < dirs[y].Length; ++x)
+                map[(uint)x, (uint)y] = dirs[y][x];
+        using var m = new MemoryStream();
+        using var w = new Utf8JsonWriter(m, new JsonWriterOptions
+        {
+            Indented = true,
+        });
+        map.Save(w);
+        w.Flush();
+        return System.Text.Encoding.UTF8.GetString(m.ToArray());
+    }
+
+    [Test]
+    public void TestSimpleSaving()
+    {
+        Assert.AreEqual("""
+            [
+                "🟨⏫⏩",
+                "⏪⏬🟨"
+            ]
+            """,
+            Store(new[]
+            {
+                new[] { Direction.None, Direction.Up, Direction.Right },
+                new[] { Direction.Left, Direction.Down, Direction.None },
+            })
+        );
+    }
+}
